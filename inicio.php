4dee9879bd46f76411b6b17b8e88c999119e4714
nathaly <nlcarvajal0@misena.edu.co>
2021-08-30 08:25:08 -0500

    formacion 30 de agosto del 2021 en el github

---
+++ inicio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>repaso logica de programacion web </title>
+</head>
+<body>
+    <?php
+    echo "comenzamos";
+    ?>
+</body>
+</html>
